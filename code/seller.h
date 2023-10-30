3695dbae2c85f23bdf838331e0aaf0aca3c234e5
acm <a-mango@users.noreply.github.com>
2023-10-30 20:16:22 +0100

    rfr: add includes to superclass

--- code/seller.h
+++ code/seller.h
@@ -6,6 +6,8 @@
 #include <map>
 #include <vector>
 #include "costs.h"
+#include <pcosynchro/pcomutex.h> // PcoMutex
+#include <mutex>                 // std::lock_guard
 
 enum class ItemType { Sand, Copper, Petrol, Chip, Plastic, Robot, Nothing};
 
